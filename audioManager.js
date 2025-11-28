eefce28c905a905067b394bf52c501b25a15d3bd
Cassetu <cassetu@example.com>
2025-11-28 10:18:54 -0800

    Audio Add (v1.0-epsilon.18)
    Signed-off-by: Cassetu <cassetu@example.com>

--- audioManager.js
+++ audioManager.js
@@ -1,18 +1,20 @@
 const AudioManager = {
     sfxEnabled: true,
+    audioCache: {},
 
     playSFX(sfxId, volume = 0.5) {
         if (!this.sfxEnabled) return;
-        const sfx = document.getElementById(sfxId);
+
+        let sfx = document.getElementById(sfxId);
+
+        if (!sfx) {
+            sfx = new Audio(sfxId);
+        }
+
         if (sfx) {
             sfx.volume = volume;
             sfx.currentTime = 0;
             sfx.play().catch(e => console.log('SFX blocked:', e));
         }
-    },
-
-    toggleSFX() {
-        this.sfxEnabled = !this.sfxEnabled;
-        return this.sfxEnabled;
     }
 };
